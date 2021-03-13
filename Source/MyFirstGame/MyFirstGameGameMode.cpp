2fd667d340de2f335a6f7e57cfeeade639b3c01c
JLenart1326 <jlenart1326@gmail.com>
2021-03-13 16:48:17 +0100

    Big Camera Change

--- Source/MyFirstGame/MyFirstGameGameMode.cpp
+++ Source/MyFirstGame/MyFirstGameGameMode.cpp
@@ -7,7 +7,7 @@
 AMyFirstGameGameMode::AMyFirstGameGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/MainCharacterBP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
